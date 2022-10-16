00805e8682c047b25b2938b814e216378067502d
Rohin Ramadass <girijas12gmail.com>
2022-10-16 13:25:44 +0100

    Verified delivery feature

--- src/main/java/pageObjects/DeliveryPage.java
+++ src/main/java/pageObjects/DeliveryPage.java
@@ -1,4 +1,13 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.support.PageFactory;
+
 public class DeliveryPage {
+
+    public DeliveryPage(WebDriver driver)
+    {
+        PageFactory.initElements(driver, this);
+    }
 }
